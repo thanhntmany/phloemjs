e7b0e1556fd2c4d0fece8feeeb1c57460613b208
Thanhntmany-Laptop <thanhntmany@gmail.com>
2023-11-26 16:38:58 +0700

    htmlbase is not needed anymore

--- htmlbase/html5.mjs
+++
@@ -1,7 +0,0 @@
-import phloemjs from "../phloe.mjs"
-
-
-export default new phloemjs.HTMLAr(
-    `<!DOCTYPE html><html lang="{{lang}}"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>{{title}}</title>{{_head_}}{{head}}</head><body {{bodyAttributes}}>{{body}}</body></html>`,
-    { lang: "en", title: "", head: [], bodyAttributes: "", body: [] }
-)
