0ac532f3878d2e9d7a2e47acd2388d84849f16e6
Thanhntmany-Laptop <thanhntmany@gmail.com>
2023-11-28 21:15:13 +0700

    add innerHead into HTMLgetRequireList()

--- phloe.mjs
+++ phloe.mjs
@@ -29,6 +29,7 @@ function mergeDeep(target, ...sources) {
 const { StringAr } = phloemjs,
     _importmap = [`<script type="importmap">`, , `</script>`],
     _mainScript = `<script type="module" src="/phloe.mjs"></script>`,
+    innerHeadRe = /<([hH][eE][aA][dD])+(\s+[^\uFDD0-\uFDEF\s\"\'\>\/\=\uFFFE\uFFFF]+(\s*\=\s*([^\s\"\'\=\<\>\`]+|\'[^\']*\'|\"[^\"]*\"))?)*\s*(\/)?>([\s\S]*?)<\/\1[^>]*>/,
     _head_ = "{{_head_}}",
     HeadTagRe = /<[hH][eE][aA][dD]+(\s+[^\uFDD0-\uFDEF\s\"\'\>\/\=\uFFFE\uFFFF]+(\s*\=\s*([^\s\"\'\=\<\>\`]+|\'[^\']*\'|\"[^\"]*\"))?)*\s*(\/)?>/g
 
@@ -49,7 +50,10 @@ const ImportmapRe = /<(script)\s+type\s*=\s*((("|')importmap\4)|importmap)\s*>([
 export { StringAr }
 export class HTMLAr extends phloemjs.StringAr {
     constructor(o = "", $ = {}) {
+        var iH = ""
         if (typeof o === 'string' || o instanceof String) {
+            iH = o.match(innerHeadRe)[6]
+
             const importmap = {}
             o = o.replace(ImportmapRe, _importmapReplacer.bind(importmap))
             if (!$.importmap) { $.importmap = importmap }
@@ -66,15 +70,20 @@ export class HTMLAr extends phloemjs.StringAr {
         }
 
         super(o, $)
+        this._iH = iH
+        this._L = new Set([_mainScript])
+        this._L1 = new Set()
         this.updateImportmap()
     }
     updateImportmap() {
+        this._L.delete(this.$._importmap_)
         const importmap = this.$.importmap; (importmap.imports || (importmap.imports = {}))["www/"] = "/"
         this.$._importmap_ = _importmap.with(1, JSON.stringify(importmap))
+        this._L.add(this.$._importmap_)
         return this
     }
     toString() {
-        this.$._head_ = [this.$._importmap_, _mainScript, this.HTMLgetRequireList()]
+        this.$._head_ = [this.$._importmap_, _mainScript, this.HTMLgetRequireList(this._L, this._L1, this._iH)]
         return super.toString()
     }
 }
